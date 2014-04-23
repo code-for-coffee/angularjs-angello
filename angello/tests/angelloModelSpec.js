f7a1afc947f0852fc87d2f3ab94c8f60333d6e45
james <james@codeforcoffee.org>
2014-04-23 14:33:00 -0500

    karma config/initial model test

---
+++ angello/tests/angelloModelSpec.js
@@ -0,0 +1,15 @@
+﻿describe('Service: angelloModel', function() {
+
+    beforeEach(module('Angello'));
+    var modelService;
+
+    beforeEach(inject(function(angelloModel) {
+        modelService = angelloModel;
+    }));
+
+    describe('#getStatus', function () {
+        it('should return seven different statuses', function() {
+            expect(modelService.getStatuses().length).toBe(7);
+        });
+    });
+});
