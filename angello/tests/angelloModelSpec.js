de296b1ad684fccf916e0ac9f7a86c6b047bd765
james <james@codeforcoffee.org>
2014-04-23 15:46:30 -0500

    more testing

--- angello/tests/angelloModelSpec.js
+++ angello/tests/angelloModelSpec.js
@@ -11,5 +11,13 @@
         it('should return seven different statuses', function() {
             expect(modelService.getStatuses().length).toBe(7);
         });
+        it('should have a status named "To Do"', function () {
+            expect(modelService.
+            getStatuses().
+            map(function (status) { // get just the name of each status
+                return status.name;
+            })).
+            toContain('To Do');
+        });
     });
 });
